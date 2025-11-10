c4c49ff5b15903e4253104ca3b306c8491009199
MasonDavies <mason.e.davies@gmail.com>
2025-11-10 17:00:59 -0600

    Started shooter subsystem

--- src/main/java/frc/robot/commands/ShooterCMD.java
+++ src/main/java/frc/robot/commands/ShooterCMD.java
@@ -1,5 +1,32 @@
 package frc.robot.commands;
 
-public class ShooterCMD {
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class ShooterCMD extends Command{
+    ShooterSubsystem shooterSubsystem;
+    Supplier<Double> leftTrigger;
+    Supplier<Double> rightTrigger;
+
+    public ShooterCMD(ShooterSubsystem shooterSubsystem, Supplier<Double> leftTrigger, Supplier<Double> rightTrigger){
+        this.shooterSubsystem = shooterSubsystem;
+    }
+
+  @Override
+  public void initialize() {}
+  
+  @Override
+  public void execute() {
     
+  }
+
+  @Override
+  public void end(boolean interrupted) {}
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 }
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,7 +1,23 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import frc.robot.Constants;
 
 public class ShooterSubsystem extends SubsystemBase {
+
+    private SparkMax flyWheel = new SparkMax(Constants.OperatorConstants.flyWheelID, MotorType.kBrushless);
+    private SparkMax transferWheel = new SparkMax(Constants.OperatorConstants.transferWheelID, MotorType.kBrushless);
+
+    public ShooterSubsystem(){
+        SparkMaxConfig config = new SparkMaxConfig();
+        config.smartCurrentLimit(35);
     
+    }
 }
