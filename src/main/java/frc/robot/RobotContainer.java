9941fa1d32741737743b161dcbb55f8835822c44
GBlast2000 <kwdierks@gmail.com>
2025-11-20 13:16:09 -0800

    deleted useless lines
    deleted 2 lines

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,9 +2,7 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.ShooterCMD;
 import frc.robot.subsystems.ShooterSubsystem;
 
